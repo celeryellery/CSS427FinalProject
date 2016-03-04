96cfbacc8b863ef260df4fd0b5f422c6dd99a042
Duncan <duncan96@users.noreply.github.com>
2016-03-04 12:47:49 -0800

    Update SonarPiano.ino

--- Melodies/SonarPiano.ino
+++ Melodies/SonarPiano.ino
@@ -1,9 +1,39 @@
 #include "pitches.h"
+/**Distances Corresponding to TonesO4 array values with current settings:
+ * 0: 15-16
+ * 1: 17-18
+ * 2: 19-20
+ * 3: 21-22
+ * 4: 23-24
+ * 5: 25
+ * 6: 26-27
+ * 7: 28-29
+ * 8: 30-31
+ * 9: 32-33
+ * 10: 34
+ * 11: 35
+ * 
+ * Beware of noise, make sure space is totally cleared
+ * and account for slow response and physical limitations
+ *
+ */
+/**Distances Corresponding to TonesO4 array values with current settings:
+ * 0: 15-16
+ * 1: 17-18
+ * 2: 19
+ * 3: 20-21
+ * 4: 22
+ * 5: 23-24
+ * 6: 25
+ * 7: 26-27
+ * 8: 28
+ * 9: 29-30
+ * 10: 31-32
+ * 11: 32-33
+ * 12: 34-35 */
 
-//Distance ThreshHold 
-int ThreshHold = 50;
-
-//Full Range of Piezo Frequencies as Pitches
+int ThreshHold_LO = 15;
+int ThreshHold_HI = 35;
 int Tones[] = {
  NOTE_B0, NOTE_C1,NOTE_CS1, NOTE_D1,NOTE_DS1,
  NOTE_E1, NOTE_F1, NOTE_FS1, NOTE_G1, NOTE_GS1,
@@ -20,38 +50,41 @@ int Tones[] = {
  NOTE_E7,NOTE_F7,NOTE_FS7,NOTE_G7,NOTE_GS7,NOTE_A7, NOTE_AS7,
  NOTE_B7,NOTE_C8,NOTE_CS8,NOTE_D8,NOTE_DS8};
 
-//One Octave of Piezo Frequencies as Pitches
-int TonesO4[] = {NOTE_C4,NOTE_CS4,
- NOTE_D4,NOTE_DS4,NOTE_E4,NOTE_F4,NOTE_FS4,NOTE_G4,NOTE_GS4, 
- NOTE_A4, NOTE_AS4,NOTE_B4};
+int TonesO4[] = {NOTE_C4,NOTE_D5,
+ NOTE_E4,NOTE_F5,NOTE_G4,NOTE_A5,NOTE_B4,NOTE_C5,NOTE_D4, 
+ NOTE_E5, NOTE_F4,NOTE_G5, NOTE_A4, NOTE_B5};
 
-//Length of Array 
+ 
 int Len = sizeof(TonesO4)/sizeof(TonesO4[1]);
 
 void setup() {
-  //Serial.begin(9600);
-  //Setup analog input pin and PWM output pin 
+  // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(A2, INPUT);
-  pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT); 
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
   int inRead = analogRead(A2);
+  /** Test / Debug
+   * Serial.print("Volt/Dist: "); 
+   * Serial.print(inRead);
+   * Serial.println(); */
   
-  //Test if input is in distance range
-  if(inRead <= ThreshHold)
+  if(inRead >= ThreshHold_LO & inRead <= ThreshHold_HI)
   {
-    //Map distance to pitch in array
-    int mapping = map(inRead, 0, ThreshHold, 0, Len-1);
+    int mapping = map(inRead, ThreshHold_LO, ThreshHold_HI, 0, Len-1);
     tone(13, TonesO4[mapping]); 
-    
-    //delay for sonar update (speed of sound) 
     delay(300);
+    /** Test / Debug
+     * Serial.print("Mapped Dist: ");
+     * Serial.print(mapping);
+     * Serial.println(); */
   }
   else
   {
-    //Cut of output
     noTone(13);
   }
-  //delay(400);
+ 
 }
